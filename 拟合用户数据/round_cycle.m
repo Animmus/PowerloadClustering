be2ef92fc5365bee903ed28c02980e14b65e32da
Animmus <70148767+Animmus@users.noreply.github.com>
2020-08-24 22:35:34 +0800

    Add files via upload

---
+++ 拟合用户数据/round_cycle.m
@@ -0,0 +1,34 @@
+function new = circle_class(old,num)
+%圆周序列位移，因为位移变化后最后面的数据补到最前面，所以使圆周移位
+%old-原始数据
+%num-移动的位数
+%new-返回移动号的数据
+[m,n] = size(old);%此时n=96而不是50！！
+k = 1;
+if num > 0
+    %num = shift(i) == 1, 将x所有元素左移一位(30%)，头尾相接，故称圆周序列位移
+    for i = num+1:n
+        new(k) = old(i);
+        k = k+1;
+    end
+    for i = 1:num
+        new(k) = old(i);
+        k = k+1;
+    end
+end
+if num < 0
+    %num = shift(i) == -1, 将x所有元素右移一位(30%)
+    for i = (num+n+1):n
+        new(k) = old (i);
+        k = k+1;
+    end
+    for i = 1:(n+num)
+        new(k) = old(i);
+        k = k+1;
+    end
+end
+if num == 0
+    %num = shift(i) == 0, 不移(40%)
+    new = old;
+end
+end
---
+++ 拟合用户数据/shift.m
@@ -0,0 +1,17 @@
+function shift = shift(num)
+%产生位移序列 shift是由不等概率生成的1行num列的含0(40%)，1(30%)，-1(30%)的向量
+%num-样本的数量
+%shift-返回每个样本的移动情况
+rands = rand(1,num);%生成1行num列(0,1)的值
+for i = 1:num
+    if rands(i)>=0 && rands(i)<=0.4
+        shift(i) = 0;
+    end
+    if rands(i)>0.4 && rands(i)<=0.7
+        shift(i) = 1;
+    end
+    if rands(i)>0.7 && rands(i)<=1
+        shift(i) = -1;
+    end
+end
+end
